bf2017b8f54631bc2a7afa2a550e5d30f37b284a
wafaaalkaddaf-afk <wafaaalkaddaf@gmail.com>
2026-08-31 12:13:48 +0300

    Sesssion11

--- Session11/script.js
+++ Session11/script.js
@@ -1,4 +1,4 @@
-courses['HTML','css','js'];
+/*courses['HTML','css','js'];
 var user=prompt("enter nameOfcourse");
 var index=courses.findIndex(cours=>cours===user);
 if(index==-1){
@@ -6,4 +6,61 @@ if(index==-1){
 }
 else{
     alert("product ia available")
+}*/
+var users=[]
+function addUser(name,id,balance){
+    var name = prompt("enter,your name")
+    var id = prompt("enter yourid")
+    var balance = Number(prompt("enter balance"))
+    var newuser={
+        name:name,
+        id:id,
+        balance:balance
+    } 
+    users.push (newuser)
+     console.log("user added success")
+} 
+function edit(id,newBalance){
+    var id = prompt("enter id edit")
+    var addbalance=Number(prompt("enter new balance"))
+var user =users.find((item)=>item.id==id)
+if (user){
+    user.balance=newBalance
+    console.log("balance updated")
+}else {
+    console.log("user not found")
 }
+}
+function transfer(fromID,toID,amount){
+    var fromID =prompt("enter remitter id ")
+    var toID=prompt("enter receiverid ")
+    var amount =Number(prompt("rnter amount "))
+    var remitter=users.find((item)=>item.id==fromID)
+    var receiver =users.find((item)=>item.id==toID)
+    if(remitter&&receiver){
+        if(remitter.balance>=amount){
+            remitter.balance=remitter.balance-amount
+        receiver.balance=receiver.balance+amount
+        console.log("transfer succcess")
+        }else{
+            console.log("failed ")
+        }
+    }
+}
+function deleteuser(id) {
+    var id =prompt("enter id to deleets")
+var user= users.find((item) => item.id == id)
+if(user){
+    users=users.filter((item)=>item.id!=id)
+    console.log("user delete success")
+}
+else{
+    console.log("not found")
+}
+}
+
+addUser()
+addUser()
+transfer()
+deleteuser()
+console.table(users)
